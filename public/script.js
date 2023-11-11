f0ba2c4b5ad7bf816aca3c7bc709e18b5cfa0935
Tao <taogit2023@gmail.com>
2023-11-12 00:23:02 +1100

    CRUD

--- public/script.js
+++ public/script.js
@@ -15,71 +15,118 @@ document.addEventListener("DOMContentLoaded", () => {
         const li = document.createElement("li");
         li.textContent = `Title:${item.title}, Instructor:${item.instructor}, Duration:${item.duration}`;
         //create a delete button
-        const deleteBtn = document.createElement('button');
-        deleteBtn.textContent = 'Delete';
-        deleteBtn.classList.add('delete-button');
-        deleteBtn.dataset.courseId = item._id
+        const deleteBtn = document.createElement("button");
+        const editBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.classList.add("delete-button");
+        deleteBtn.dataset.courseId = item._id;
+        editBtn.textContent = "Edit";
+        editBtn.classList.add("edit-button");
+        editBtn.dataset.courseId = item._id;
         li.append(deleteBtn);
+        li.append(editBtn);
         courseList.append(li);
       });
     } catch (error) {
       console.log("error fetching courses", error);
     }
   };
-  addCourseForm.addEventListener("submit",async (event)=>{
+  addCourseForm.addEventListener("submit", async (event) => {
     event.preventDefault();
     const formData = new FormData(addCourseForm);
-    const courseData = Object.fromEntries(formData)
-    console.log(courseData)
+    const courseData = Object.fromEntries(formData);
+    console.log(courseData);
     try {
-        const url = "http://localhost:8000/api/courses"
-        const response = await fetch(url, {
-            method: 'POST',
-            headers:{
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(courseData)
+      const url = "http://localhost:8000/api/courses";
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(courseData),
+      });
+      if (response.ok) {
+        addCourseForm.reset();
+        fetchData();
+      }
+      const responseData = await response.json();
+      if (responseData.errors) {
+        errorContainer.innerHTML = "";
+        for (const field in responseData.errors) {
+          const errorText = responseData.errors[field];
+          const errorElement = document.createElement("p");
+          errorElement.textContent = errorText;
+          errorContainer.appendChild(errorElement);
         }
-        )
-        if(response.ok){
-            addCourseForm.reset();
-            fetchData();
-        }
-        const responseData  = await response.json();
-        if(responseData.errors){
-            errorContainer.innerHTML = '';
-            for(const field in responseData.errors){
-                const errorText = responseData.errors[field];
-                const errorElement = document.createElement('p');
-                errorElement.textContent = errorText;
-                errorContainer.appendChild(errorElement)
-            }
-        }
+      }
     } catch (error) {
-        console.error('Error adding course',response.statusText);
-        
+      console.error("Error adding course", response.statusText);
     }
-  })
-  courseList.addEventListener('click',async (event)=>{
-    if(event.target.classList.contains('delete-button')){
+  });
+  courseList.addEventListener("click", async (event) => {
+    if (event.target.classList.contains("delete-button")) {
       const courseId = event.target.dataset.courseId;
-      if(confirm("are you sure you want to delete this course?")){
+      if (confirm("are you sure you want to delete this course?")) {
         try {
           const response = await fetch(
-            `http://localhost:8000/api/courses/${courseId}`,{
-              method: "DELETE"
+            `http://localhost:8000/api/courses/${courseId}`,
+            {
+              method: "DELETE",
             }
           );
-          if(response.ok){
+          if (response.ok) {
             fetchData();
-          }else{
-            console.error('Error deleting course:', response.statusText)
+          } else {
+            console.error("Error deleting course:", response.statusText);
           }
         } catch (error) {
-          console.error(error)
+          console.error(error);
         }
       }
     }
-  })
+    if (event.target.classList.contains("edit-button")) {
+      const courseId = event.target.dataset.courseId;
+      const editFrom = document.createElement("form");
+      const titleInput = document.createElement("input");
+      titleInput.name = "title";
+      const instructorInput = document.createElement("input");
+      instructorInput.name = "instructor";
+      const durationInput = document.createElement("input");
+      durationInput.name = "duration";
+      durationInput.placeholder = "duration";
+      const descriptionInput = document.createElement("input");
+      descriptionInput.name = "description";
+      const saveBtn = document.createElement("button");
+      saveBtn.textContent = "Save";
+      saveBtn.type = "submit";
+      saveBtn.classList.add("save-button");
+      editFrom.appendChild(titleInput);
+      editFrom.appendChild(instructorInput);
+      editFrom.appendChild(descriptionInput);
+      editFrom.appendChild(durationInput);
+      editFrom.appendChild(saveBtn);
+      courseList.appendChild(editFrom);
+      try {
+        editFrom.addEventListener("submit", async (event) => {
+          event.preventDefault();
+          const formData = new FormData(editFrom);
+          const courseData = Object.fromEntries(formData);
+          console.log(courseData);
+          const response = await fetch(
+            `http://localhost:8000/api/courses/${courseId}`,
+            {
+              method: "PUT",
+              body: JSON.stringify(courseData),
+            }
+          );
+          if (response.ok) {
+            fetchData();
+          } else {
+            console.error("error");
+          }
+        });
+      } catch (error) {}
+    }
+  });
   fetchData();
 });
